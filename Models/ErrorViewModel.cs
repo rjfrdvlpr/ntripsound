230c85e862419550e8eee57047304d3192e020c7
Frdvlpr <jampierts@gmail.com>
2021-11-03 20:08:33 -0500

    updated project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ntripsound.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
